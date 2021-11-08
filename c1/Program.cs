92ecfeeb5e1e7af4c336f5ca625e72a5fa7c88b7
Matheus Brant <matheusbrantgo@gmail.com>
2021-11-08 15:54:54 -0300

    treinando build run

--- c1/Program.cs
+++ c1/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace c1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace c1{
+    class Program{
+        static void Main(string[] args){
+            int contador = 5;
+
+            for(int i=0;i<contador;i++){
+                Console.WriteLine($"salve {i}");
+            }
         }
     }
 }
